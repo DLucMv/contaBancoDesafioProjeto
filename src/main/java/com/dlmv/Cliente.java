3680ad65620a969c48ce98db839c041782c71d4e
DLucMv <dlucasmv@gmail.com>
2023-05-28 16:47:53 -0300

    classe cliente criada

---
+++ src/main/java/com/dlmv/Cliente.java
@@ -0,0 +1,43 @@
+package com.dlmv;
+
+public class Cliente {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public Cliente() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
--- src/main/java/com/dlmv/ContaTerminal.java
+++ src/main/java/com/dlmv/ContaTerminal.java
@@ -5,7 +5,37 @@ import javax.swing.*;
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        
+        Cliente cliente = new Cliente();
+
+        JOptionPane.showMessageDialog(null,"""
+                Olá! Seja bem vindo.
+                Vamos lhe auxiliar em seu cadastro.
+                """);
+
+        cliente.setNumero(Integer.parseInt(JOptionPane.showInputDialog(null,
+                "Por favor, comece inserindo o número de sua conta:"
+        )));
+
+        cliente.setAgencia(JOptionPane.showInputDialog(null,
+                "Agora, insira o número de sua agência bancária:"
+        ));
+
+        cliente.setNomeCliente(JOptionPane.showInputDialog(null,
+                "Por favor, digite o seu nome:"
+        ));
+
+        cliente.setSaldo(Double.parseDouble(JOptionPane.showInputDialog(null,
+                "Qual será o valor de seu depósito inicial?"
+        )));
+
+        JOptionPane.showMessageDialog(null,
+                "Numero da conta: " + cliente.getNumero()
+                        + "\nAgência: " + cliente.getAgencia()
+                        + "\nNome do cliente: " + cliente.getNomeCliente()
+                        + "\nSaldo: " + cliente.getSaldo());
+
+
+        System.exit(0);
 
     }
 }
